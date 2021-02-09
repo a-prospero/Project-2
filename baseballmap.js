f3025e82ce0bf1753520e05acb352bc3354dc2be
Erik Thompson <erik.thompson@outlook.com>
2021-02-09 15:04:38 -0700

    add all map layers into one documant

--- baseballmap.js
+++ baseballmap.js
@@ -1,81 +1,137 @@
 // Create variables for data files
 var queryUrl = "fields.json";
 var baseball_url = 'static/data/baseball_stats.csv';
+var football_url = 'stadiums.json';
+var hockey_url = "nhl-stadiums.json";
 // Create variarable for icon
 var diamondIcon = L.icon({iconUrl: 'images/diamond.png',
-  iconSize: [25,25]})
-// Perform a GET request to the query URL and send data.features to createFeatures function
-d3.json(queryUrl, function(data) {
-  createFeatures(data.features);
+  iconSize: [25,25]
   });
-// Define a function we want to run once for each feature in the features array
-function createFeatures(ballData) {
-  var baseball = L.geoJson(ballData  ,{
-    pointToLayer: function(feature,latlng){
-      return L.marker(latlng,{icon: diamondIcon});
-    }
-  })
-  createMap(baseball);
-}
-
-function createMap(baseball) {
-
-  // Define streetmap layer
-  var streetmap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
-    attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
-    tileSize: 512,
-    maxZoom: 18,
-    zoomOffset: -1,
-    id: "mapbox/streets-v11",
-    accessToken: API_KEY
+var footballIcon = L.icon({
+  iconUrl: 'images/football.png',
+  iconSize: [25, 25],
   });
+var hockeyIcon = L.icon({
+    iconUrl: 'images/hockey_puck.png',
+    iconSize: [25, 25],
+});
 
-  // Define darkmap layer
-  var darkmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
-    attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
-    maxZoom: 18,
-    id: "dark-v10",
-    accessToken: API_KEY
-  });
+var layers = {
+    Baseball: new L.LayerGroup(),
+    Football: new L.LayerGroup(),
+    Hockey: new L.LayerGroup()
+};
 
-  // Define a baseMaps object to hold our base layers
-  var baseMaps = {
-    "Street Map": streetmap,
-    "Dark Map": darkmap
-  };
+var streetmap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+tileSize: 512,
+maxZoom: 18,
+zoomOffset: -1,
+id: "mapbox/streets-v11",
+accessToken: API_KEY
+});
 
-  // Create overlay object to hold our overlay layer
-  var overlayMaps = {
-    Baseball: baseball
-  };
+// Define darkmap layer
+var darkmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+maxZoom: 18,
+id: "dark-v10",
+accessToken: API_KEY
+});
 
-  // Create our map, giving it the streetmap and earthquakes layers to display on load
-  var myMap = L.map("map", {
-    center: [
-      37.09, -95.71
-    ],
-    zoom: 4,
-    layers: [streetmap, baseball]
-  });
-  
-    var supportsPassive = false;
-  try {
+// Define a baseMaps object to hold our base layers
+var baseMaps = {
+"Street Map": streetmap,
+"Dark Map": darkmap
+};
+
+// Create overlay object to hold our overlay layer
+var overlayMaps = {
+    "Baseball": layers.Baseball, 
+    "Football": layers.Football,
+    "Hockey": layers.Hockey
+};
+
+// Create our map, giving it the streetmap and earthquakes layers to display on load
+var myMap = L.map("map", {
+center: [
+37.09, -95.71
+],
+zoom: 4,
+layers: [streetmap, layers.Baseball, layers.Football, layers.Hockey]
+});
+
+var supportsPassive = false;
+try {
     var opts = Object.defineProperty({}, 'passive', {
-      get: function() {
-        supportsPassive = true;
-      }
+        get: function() {
+            supportsPassive = true;
+        }
     });
     window.addEventListener("testPassive", null, opts);
     window.removeEventListener("testPassive", null, opts);
-  } catch (e) {}
+} catch (e) {}
 
-  // Create a layer control with baseMaps and overlayMaps, add to map
-  L.control.layers(baseMaps, overlayMaps, {
-    collapsed: false
-  }).addTo(myMap);
+// Create a layer control with baseMaps and overlayMaps, add to map
+L.control.layers(baseMaps, overlayMaps, {
+collapsed: false
+}).addTo(myMap);
 
-  // Read and parse data from baseball database
-  function getData() {
+//Define a function we want to run once for each feature in the features array, add layer to layer control
+function createBaseballFeatures(baseballData) {
+    L.geoJson(baseballData  ,{
+        pointToLayer: function(feature,latlng){
+            return L.marker(latlng,{icon: diamondIcon});
+            }
+        }).addTo(layers.Baseball)
+}
+
+//Run function to add baseball stadiums to map
+d3.json(queryUrl, function(data) {
+    createBaseballFeatures(data.features);
+});
+
+// Define a function we want to run once for each feature in the features array, give each feature a popup describing the team and stadium
+function createFootballFeatures(footballData) {
+    function onEachFeature(feature, layer) {
+        L.Icon.Default.prototype.options.iconSize='0'
+        var marker = L.marker([feature.geometry.coordinates[1],feature.geometry.coordinates[0]], {icon: footballIcon})
+        // marker.addTo(layers.Football)
+        marker.bindPopup("<h3>" + (feature.properties.Stadium) + "</h3><hr><p>" + (feature.properties.Team) + " (" + feature.properties.Conference + ")" 
+        + "</p><hr><p>" + "Wins at home: " + (feature.properties.Homefield_Wins) + "</p> \r\n" + "<p>" + "Homefield Advantage Ranking: "+ (feature.properties.Homefield_adv_rank) + "</p>");
+        marker.bindTooltip( `Team: ${feature.properties.Team}<br>
+                          Home Wins: ${feature.properties.Homefield_Wins}<br>
+                          Home Losses: N/A <br>
+                          Home Win Percentage: N/A`).addTo(layers.Football);
+        }
+        L.geoJSON(footballData, {
+            onEachFeature: onEachFeature
+            }).addTo(layers.Football)
+}
+  
+//Run function to add football stadiums to map
+d3.json(football_url, function(data2) {
+  createFootballFeatures(data2.features);
+});
+//Define a function for each feature, add popup and tooltip to each hockey rink
+function createHockeyFeatures(hockeyData) {
+    
+function onEachFeature(feature, layer) {
+      var marker = L.marker([feature.geometry.coordinates[1], feature.geometry.coordinates[0]],{icon: hockeyIcon})
+      marker.bindTooltip("<h3>" + feature.properties.name +
+        "</h3><hr><p>" + (feature.properties.team) + "</p>").addTo(layers.Hockey);
+    }  
+    L.geoJSON(hockeyData, {
+        onEachFeature: onEachFeature
+        }).addTo(layers.Hockey)
+}
+
+d3.json(hockey_url, function(data) {
+  createHockeyFeatures(data.features);
+});
+
+// Read and parse data from baseball database
+function getData() {
     d3.csv(baseball_url, function(game_data) {
         h_wins=[];
         h_losses =[];
@@ -94,7 +150,7 @@ function createMap(baseball) {
                 h_losses.push(game_data[i].h_name)
             }
         }
-        
+
         // Break down wins and losses, home and away, by team
         h_wins.sort()
         h_losses.sort()
@@ -107,7 +163,7 @@ function createMap(baseball) {
                 home_wins[h_wins[i]] = 0;
             ++home_wins[h_wins[i]];
         }
-        
+
         home_losses = { };
         for(var i = 0; i < h_losses.length; ++i) {
             if(!home_losses[h_losses[i]])
@@ -131,12 +187,12 @@ function createMap(baseball) {
 
         // List all teams in the order they appear in geoJSON
         teams = ["Los Angeles Angels","Arizona Diamondbacks","Atlanta Braves","Baltimore Orioles","Boston Red Sox","Chicago White Sox",
-                 "Chicago Cubs","Cincinnati Reds","Cleveland Indians","Colorado Rockies","Detroit Tigers","Florida Marlins","Houstin Astros",
-                 "Kansas City Royals","Los Angeles Dodgers","Miami Marlins","Milwaukee Brewers","Minnesota Twins","Arizona Diamondbacks",
-                 "New York Yankees","New York Mets","Oakland Athletics","Philadelphia Phillies","Pittsburgh Pirates","San Diego Padres",
-                 "Seattle Mariners","San Francisco Giants","St. Louis Cardinals","Tampa Bay Rays","Texas Rangers","Toronto Bluejays",
-                 "Washington Nationals"]
-         
+                    "Chicago Cubs","Cincinnati Reds","Cleveland Indians","Colorado Rockies","Detroit Tigers","Florida Marlins","Houstin Astros",
+                    "Kansas City Royals","Los Angeles Dodgers","Miami Marlins","Milwaukee Brewers","Minnesota Twins","Arizona Diamondbacks",
+                    "New York Yankees","New York Mets","Oakland Athletics","Philadelphia Phillies","Pittsburgh Pirates","San Diego Padres",
+                    "Seattle Mariners","San Francisco Giants","St. Louis Cardinals","Tampa Bay Rays","Texas Rangers","Toronto Bluejays",
+                    "Washington Nationals"]
+            
         var records = [];
         for (var i = 0; i<teams.length; ++i) {    
             records[i] = {
@@ -156,55 +212,13 @@ function createMap(baseball) {
         
         // Iterate through records objects and create tooltips and add to baseball layer
         for(var i = 1; i < 32; i++) {
-          if (i===2) { continue; }
-          var marker = L.marker([overlayMaps.Baseball._layers[i]._latlng.lat, overlayMaps.Baseball._layers[i]._latlng.lng],{icon: diamondIcon})
-          marker.bindTooltip(`Team: ${records[order[i]].team}<br>
-                              Home Wins: ${records[order[i]].home_wins}<br>
-                              Home Losses: ${records[order[i]].home_losses}<br>
-                              Home Win Percentage: ${records[order[i]].home_win_pct}%`).addTo(overlayMaps.Baseball)
+            if (i===2) { continue; }
+            var marker = L.marker([overlayMaps.Baseball._layers[41]._layers[(i+39)]._latlng.lat, overlayMaps.Baseball._layers[41]._layers[(i+39)]._latlng.lng],{icon: diamondIcon})
+            marker.bindTooltip(`Team: ${records[order[i]].team}<br>
+                                Home Wins: ${records[order[i]].home_wins}<br>
+                                Home Losses: ${records[order[i]].home_losses}<br>
+                                Home Win Percentage: ${records[order[i]].home_win_pct}%`).addTo(layers.Baseball)
         }
-      
-        var ctx = document.getElementById('chart').getContext('2d');
-var myChart = new Chart(ctx, {
-    type: 'bar',
-    data: {
-        labels: records[teams[order[i]]],
-        datasets: [{
-            label: 'Teams',
-            data: [12, 19, 3, 5, 2, 3],
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
-            borderWidth: 1
-        }]
-    },
-    options: {
-        scales: {
-            yAxes: [{
-                ticks: {
-                    beginAtZero: true
-                }
-            }]
-        }
-    }
-});
-      
-      
-      })
-  }
-  getData();
+    });
 }
-
+getData();
