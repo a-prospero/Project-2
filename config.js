78904ebc7bbe09507b9c9f83bbc1c1be1e4e22dd
april Prospero Garciagit config --global user.email april.prospero@gmail.com <april.prospero@gmail.com>
2021-02-11 06:31:41 -0700

    merge

--- config.js
+++ config.js
@@ -1,11 +1,4 @@
-<<<<<<< HEAD
-// API key
-const API_KEY = "pk.eyJ1IjoiYXByb3NwZXJvIiwiYSI6ImNranZoeWJkbTA0MmYydm8yZWF6N3JiNnAifQ.6IHhn0-sB4Ly3I07lBGUcQ"
-=======
+  
 //API key
 //peter's leaflet api key
 const API_KEY = "sk.eyJ1IjoicHdpdHdlcjkzIiwiYSI6ImNrandhdzh3ejB5eW0ycXAzNzUybjZtdDUifQ.3dvy346rOozHEnKI6hr2lg"
-<<<<<<< HEAD:static/js/config.js
->>>>>>> 96e17be6759ab0b7f4d74ee6538f2d01aebe6744
-=======
->>>>>>> 4563085f1a683718e6ec7db7be6447ed09a5950e:config.js
