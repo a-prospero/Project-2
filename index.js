5e1d63fe12d19b3554a910af9b894e58fd80490e
april Prospero Garciagit config --global user.email april.prospero@gmail.com <april.prospero@gmail.com>
2021-02-11 09:45:46 -0700

    nfl chart

---
+++ index.js
@@ -0,0 +1,40 @@
+fetch('/nfl_work/transformed_nfl_data.csv')
+   .then(function (response) {
+      return response.text();
+   })
+   .then(function (text) {
+    let series = csvToSeries(text);
+    renderChart(series);
+    console.log(series);
+})
+   .catch(function (error) {
+      //Something went wrong
+      console.log(error);
+   });
+   function csvToSeries(text) {
+	const winPercentage = 'Home_Win_Percentage';
+	let dataAsJson = JSC.csv2Json(text);
+	let wins = [], losses = [];
+	dataAsJson.forEach(function (row) {
+		 //add either to male, female, or discard.
+         if (row.teams === '49ers','Bears', 'Bengals', 'Bills', 'Broncos', 'Browns', 'Buccaneers', 'Cardinals', 'Chargers', 'Chiefs', 'Colts', 'Cowboys', 'Dolphins', 'Eagles', 'Falcons', 'Giants', 'Jaquars', 'Jets', 'Lions', 'Packers', 'Panthers', 'Patriots', 'Raiders', 'Rams', 'Ravens', 'Redskins', 'Saints', 'Seahawks', 'Steelers', 'Texans', 'Titans', 'Vikings' ) {
+			if (row.wins >0) {
+				wins.push({x: row.teams, y: row["wins"]});
+			} else if (row.losses >0) {
+				losses.push({x: row.teams, y: row["losses"]});
+			}
+		}    
+	});
+    return [
+        {name: 'Wins', points: wins},
+        {name: 'Losses', points: losses}
+    ];
+    console.log([wins,losses]);
+}
+
+function renderChart(series){
+    JSC.Chart('chartDiv', {
+       series: series
+    });
+}
+
